0b4fdc980dc9591dc8878e3bed3bd71aa4aedfd3
benga20 <bengadot@walla.com>
2016-03-04 23:41:38 +0200

    a

--- server.py
+++ server.py
@@ -8,11 +8,12 @@ api = application = falcon.API()
 login_res = login.Login()
 register_res = login.Register()
 friendship_res = friendship.Friendship()
+friendship_response_res = friendship.FriendshipResponse()
 
 api.add_route('/api/login', login_res)
 api.add_route('/api/register', register_res)
 api.add_route('/api/friendship', friendship_res)
-api.add_route('/api/friendship/response', friendship_res)
+api.add_route('/api/friendship/response', friendship_response_res)
 
 if __name__ == '__main__':
     httpd = simple_server.make_server('127.0.0.1', 8000, api)
