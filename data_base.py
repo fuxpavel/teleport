f0b641bb80801740dfe5e310c483f854d09c43a3
benga20 <bengadot@walla.com>
2016-03-05 23:30:46 +0200

    wfwejhfwe

--- data_base.py
+++ data_base.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import random
 
 Base = declarative_base()
 DB_URL = 'sqlite:///Teleport_DB.db'
@@ -25,6 +26,7 @@ class User(Base):
     username = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     ip = Column(String)
+    token = Column(String, unique=True)
 
     def init_data_base(self, engine=None):
         engine = engine if engine else get_engine()
@@ -41,13 +43,43 @@ class User(Base):
         return True
 
     def login_user(self, username, password, engine=None):
-        info = self.check_exist_user(username, engine)
+        info = self.check_exist_user_username(username, engine)
         if info and info[0].password == password:
-            return True
+            return self.set_token(username, engine)
         else:
             return False
 
-    def check_exist_user(self, username, engine=None):
+    def get_username_by_token(self, token, engine=None):
+        session = get_session(engine)
+        if self.check_exist_user_token(token):
+            return session.query(User).filter_by(token=token).all()[0].username
+        else:
+            return False
+
+    def get_token_by_username(self, username, engine=None):
+        session = get_session(engine)
+        if self.check_exist_user_username(username):
+            return session.query(User).filter_by(username=username).all()[0].token
+        else:
+            return False
+
+    def set_token(self, username, engine=None):
+        token = str(random.randint(1, 1000))
+        session = get_session(engine)
+        if session.query(User).filter_by(username=username).update({"token": token}):
+            try:
+                session.commit()
+            except:
+                return False
+            return token
+        else:
+            return False
+
+    def check_exist_user_token(self, token, engine=None):
+        session = get_session(engine)
+        return session.query(User).filter_by(token=token).all()
+
+    def check_exist_user_username(self, username, engine=None):
         session = get_session(engine)
         return session.query(User).filter_by(username=username).all()
 
@@ -66,14 +98,14 @@ class User(Base):
         session = get_session(engine)
         info = session.query(User).all()
         for i in info:
-            print str(i.id)+")", i.username, i.password, i.ip
+            print str(i.id)+")", i.username, i.password, i.ip, i.token
 
 
 class FriendRequest(Base):
     __tablename__ = 'Friend_request'
     id = Column(Integer, primary_key=True)
-    sender = Column(String, ForeignKey(User.username))
-    reply = Column(String, ForeignKey(User.username))
+    sender = Column(String, ForeignKey(User.token))
+    reply = Column(String, ForeignKey(User.token))
     status = Column(String)
 
     def init_data_base(self, engine=None):
@@ -114,7 +146,7 @@ class FriendRequest(Base):
         session = get_session(engine)
         db = User()
         f = Friendship()
-        if db.check_exist_user(sender, engine) and db.check_exist_user(reply, engine):
+        if db.check_exist_user_token(sender, engine) and db.check_exist_user_token(reply, engine):
             if not f.check_friendship(sender, reply, engine) and not self.check_friend_request(sender, reply, engine):
                 new_request = FriendRequest(sender=sender, reply=reply)
                 session.add(new_request)
@@ -145,13 +177,13 @@ class FriendRequest(Base):
 class Friendship(Base):
     __tablename__ = 'Friendship'
     id = Column(Integer, primary_key=True)
-    friend1 = Column(String, ForeignKey(User.username))
-    friend2 = Column(String, ForeignKey(User.username))
+    friend1 = Column(String, ForeignKey(User.token))
+    friend2 = Column(String, ForeignKey(User.token))
 
     def create_friendship(self, friend1, friend2, engine=None):
         session = get_session(engine)
         db = User()
-        if db.check_exist_user(friend1, engine) and db.check_exist_user(friend2, engine):
+        if db.check_exist_user_token(friend1, engine) and db.check_exist_user_token(friend2, engine):
             if not self.check_friendship(friend1, friend2, engine):
                 new_friendship = Friendship(friend1=friend1, friend2=friend2)
                 session.add(new_friendship)
